14e9a5205681d7f1032616095876faa0cdf044a8
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-09-24 16:34:41 +0530

    Update NovalnetInvoicePaymentMethod.php

--- src/Methods/NovalnetInvoicePaymentMethod.php
+++ src/Methods/NovalnetInvoicePaymentMethod.php
@@ -22,6 +22,7 @@ use Novalnet\Helper\PaymentHelper;
 use Novalnet\Services\PaymentService;
 use Plenty\Modules\Basket\Models\Basket;
 use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
+use Plenty\Plugin\Log\Loggable;
 
 /**
  * Class NovalnetPaymentMethod
@@ -30,6 +31,7 @@ use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
  */
 class NovalnetInvoicePaymentMethod extends PaymentMethodService
 {
+   use Loggable;
     /**
      * @var ConfigRepository
      */
@@ -154,8 +156,9 @@ class NovalnetInvoicePaymentMethod extends PaymentMethodService
      *
      * @return bool
      */
-    public function isSwitchableFrom(): bool
+    public function isSwitchableFrom($orderId): bool
     {
+	    $this->getLogger(__METHOD__)->error('CM order ID', $orderId);
 	    return true;
     }
 }
